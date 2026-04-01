011d8b0daa9b4900f2a20a6866c062f67ae6edc7
dayvidch <davidcha2006@gmail.com>
2026-04-01 00:50:22 -0700

    class 2

---
+++ Class Modules/class2.py
@@ -0,0 +1,10 @@
+'''
+Use a while loop.  
+Ask the user to enter a number greater than zero.  
+If the user doesn't do this, display an error message and ask the user to try again.  Continue until the user is successful.
+'''
+x = 0
+while x <= 0:
+    x =  float(input("enter a number: "))
+
+print("done")
